57417aa09ea1f329ee50b3c9b0e5ec785ceb4f67
Jan Hajek <jan.hajek@outreach.io>
2023-10-13 18:24:10 +0200

    fix

--- pkg/ai/strategy/knn/test_model.go
+++ pkg/ai/strategy/knn/test_model.go
@@ -3,15 +3,20 @@ package knn
 import (
 	"context"
 	"fmt"
+	"runtime"
+	"sync"
+
 	"github.com/jan-hajek/ai/pkg/ai/csvx"
+	"github.com/jan-hajek/ai/pkg/worker"
+	"github.com/schollz/progressbar/v3"
 )
 
-type mistakeDetails struct{
-	path string
-correct int
-guessed int
-probGuessed float64
-secondBest int
+type mistakeDetails struct {
+	path        string
+	correct     int
+	guessed     int
+	probGuessed float64
+	secondBest  int
 }
 
 func (b *KnnStrategy) TestModel(ctx context.Context) error {
@@ -44,46 +49,81 @@ func (b *KnnStrategy) TestModel(ctx context.Context) error {
 
 	curMistakesToDisplay := 0
 
-	for _, item := range testData {
-		neighbors := nearestNeighbors(k, item, trainData)
+	fmt.Printf("Testing model with k: %d, test set: %d, train set: %d \n", k, len(testData), len(trainData))
 
-		number, numberCount, secondBest := guessNumbers(neighbors[:k])
+	bar := progressbar.Default(int64(len(testData)))
 
-		totalCountsPerNum[item.number]++
+	var mx sync.Mutex
 
-			if number == item.number {
-				correctGuessCount++
-				correctPositiveGuessCount[number]++
-			} else {
-				for i:=0;i<10;i++ {
-					if i != number {
-						correctNegativeGuessCount[number]++
+	worker.ProcessInParallel(
+		ctx,
+		testData,
+		func(ctx context.Context, item Item) (interface{}, error) {
+			neighbors := nearestNeighbors(k, item, trainData)
+
+			number, numberCount, secondBest := guessNumbers(neighbors[:k])
+
+			func() {
+				mx.Lock()
+				defer mx.Unlock()
+
+				totalCountsPerNum[item.number]++
+
+				if number == item.number {
+					correctGuessCount++
+					correctPositiveGuessCount[number]++
+				} else {
+					for i := 0; i < 10; i++ {
+						if i != number {
+							correctNegativeGuessCount[number]++
+						}
+					}
+					// create mistake example
+					if curMistakesToDisplay < maxMistakesToDisplay {
+						exampleOfMistakes[curMistakesToDisplay] = mistakeDetails{
+							path:        item.sourceFileName,
+							correct:     item.number,
+							guessed:     number,
+							probGuessed: float64(numberCount) / float64(k),
+							secondBest:  secondBest,
+						}
+						curMistakesToDisplay++
 					}
 				}
-				// create mistake example
-				if curMistakesToDisplay < maxMistakesToDisplay {
-					exampleOfMistakes[curMistakesToDisplay] = mistakeDetails{
-						path: item.sourceFileName,
-						correct: item.number,
-						guessed: number,
-						probGuessed: float64(numberCount)/float64(k),
-						secondBest: secondBest,
-					}
-					curMistakesToDisplay++
-				}
-			}
-	}
+			}()
 
-	accuracy := float64(correctGuessCount)/float64(len(testDataCsv))
+			bar.Add(1)
 
-	fmt.Printf("Accuracy: k=%d, accuracy=%.2f%%\n", k, accuracy)
-	for i:=0; i<10;i++ {
-	fmt.Printf("Num=%d: TotalCount=%d, Sensitivity=%.2f%%, Specificity=%.2f%%\n", i,
-		totalCountsPerNum[i], float64(correctPositiveGuessCount[i])/float64(totalCountsPerNum[i]), float64(correctNegativeGuessCount[i])/float64((len(testDataCsv))-totalCountsPerNum[i]))
+			return nil, nil
+		},
+		worker.WithWorkersCount(runtime.NumCPU()),
+	)
+
+	bar.Exit()
+
+	fmt.Printf(
+		"Accuracy: k=%d, accuracy=%.2f%%\n",
+		k,
+		float64(correctGuessCount)/float64(len(testDataCsv))*100,
+	)
+	for i := 0; i < 10; i++ {
+		fmt.Printf(
+			"Num=%d: TotalCount=%d, Sensitivity=%.2f%%, Specificity=%.2f%%\n",
+			i,
+			totalCountsPerNum[i],
+			float64(correctPositiveGuessCount[i])/float64(totalCountsPerNum[i])*100,
+			float64(correctNegativeGuessCount[i])/float64((len(testDataCsv))-totalCountsPerNum[i])*100,
+		)
 	}
-	for i:=0; i<maxMistakesToDisplay;i++ {
-		fmt.Printf("Mistakes examples:\n %s, Correct=%d, Guessed=%d, ProbGuessed=%.2f%%, SecondBest=%d\n",
-			exampleOfMistakes[i].path, exampleOfMistakes[i].correct, exampleOfMistakes[i].guessed, exampleOfMistakes[i].probGuessed, exampleOfMistakes[i].secondBest)
+	for i := 0; i < maxMistakesToDisplay; i++ {
+		fmt.Printf(
+			"Mistakes examples:\n %s, Correct=%d, Guessed=%d, ProbGuessed=%.2f%%, SecondBest=%d\n",
+			exampleOfMistakes[i].path,
+			exampleOfMistakes[i].correct,
+			exampleOfMistakes[i].guessed,
+			exampleOfMistakes[i].probGuessed*100,
+			exampleOfMistakes[i].secondBest,
+		)
 	}
 	return nil
 }
